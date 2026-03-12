d7ab368ff6b806c7a3dcd00cfad7bca7823ae8dd
GKalogerA <agkaloger@uoi.gr>
2026-03-12 10:09:00 +0200

    Update ELKE_FTE.py

--- ELKE_FTE.py
+++ ELKE_FTE.py
@@ -177,20 +177,126 @@ u[7]=FTE_Count_QS(ED)
 u[8]=FTE_Count_QS(TOT)
 u[9]=sum(u[:8])
 
+AH_Res=[]
+CH_Res=[]
+LS_Res=[]
+EN_Res=[]
+CS_Res=[]
+PS_Res=[]
+BS_Res=[]
+ED_Res=[]
+OTHER_Res=[]
+AH_Ind=[]
+CH_Ind=[]
+LS_Ind=[]
+EN_Ind=[]
+CS_Ind=[]
+PS_Ind=[]
+BS_Ind=[]
+ED_Ind=[]
+TOT_Ind=[]
+for i in range(N1):
+   # df_start1=datetime.datetime.strptime(data1[i][4],"%m/%d/%Y")#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+   # df_end1=datetime.datetime.strptime(data1[i][5],"%m/%d/%Y")#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+   # x1=np.timedelta64(df_start1-date_start,"D").astype(int)#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+   # y1=np.timedelta64(date_end-df_end1,"D").astype(int)#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+   # tot1=np.timedelta64(df_end1-df_start1,"D").astype(int)#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+   # check11=np.timedelta64(df_end1-date_start,"D").astype(int)#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+   # check21=np.timedelta64(df_start1-date_end,"D").astype(int)#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+    #if check11<0 or check21>0:#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+    #    data1[i][2]=0#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+    #else:#Αν θέλουμε να "χωρίσουμε" ημερομηνίες
+        #Διαχωρίζει καθε συμβαλλόμενο στην σχολή του ως προς το FTE
+    if data1[i][1] in set_AH:
+        AH_Res.append(data1[i][2])
+    if data1[i][1] in set_CH:
+        CH_Res.append(data1[i][2])
+    if data1[i][1] in set_LS:
+        LS_Res.append(data1[i][2])
+    if data1[i][1] in set_EN:
+        EN_Res.append(data1[i][2])
+    if data1[i][1] in set_CS:
+        CS_Res.append(data1[i][2])
+    if data1[i][1] in set_PS:
+        PS_Res.append(data1[i][2])
+    if data1[i][1] in set_BS:
+        BS_Res.append(data1[i][2])
+    if data1[i][1] in set_ED:
+        ED_Res.append(data1[i][2])
+    if data1[i][1] in set_OTHER:
+        OTHER_Res.append(data1[i][2])
+for j in range(N2):
+    #df_start2=datetime.datetime.strptime(data2[j][4],"%m/%d/%Y")
+    #df_end2=datetime.datetime.strptime(data2[j][5],"%m/%d/%Y")
+    #x2=np.timedelta64(df_start2-date_start,"D").astype(int)
+    #y2=np.timedelta64(date_end-df_end2,"D").astype(int)
+    #tot2=np.timedelta64(df_end2-df_start2,"D").astype(int)
+    #check12=np.timedelta64(df_end2-date_start,"D").astype(int)
+    #check22=np.timedelta64(df_start2-date_end,"D").astype(int)
+    #if check12<0 or check22>0:
+    #    data2[j][2]=0
+    #else:
+        #Διαχωρίζει καθε συμβαλλόμενο στην σχολή του ως προς το FTE
+    TOT_Ind.append(data2[j][2])
+    if data2[j][1] in set_AH:
+        AH_Ind.append(data2[j][2])
+    if data2[j][1] in set_CH:
+        CH_Ind.append(data2[j][2])
+    if data2[j][1] in set_LS:
+        LS_Ind.append(data2[j][2])
+    if data2[j][1] in set_EN:
+        EN_Ind.append(data2[j][2])
+    if data2[j][1] in set_CS:
+        CS_Ind.append(data2[j][2])
+    if data2[j][1] in set_PS:
+        PS_Ind.append(data2[j][2])
+    if data2[j][1] in set_BS:
+        BS_Ind.append(data2[j][2])
+    if data2[j][1] in set_ED:
+        ED_Ind.append(data2[j][2])
+    
+print(TOT_Ind)
+uu=np.zeros(10)
+vv=np.zeros(10)
+vv[0]=sum(AH_Ind)
+vv[1]=sum(CH_Ind)
+vv[2]=sum(LS_Ind)
+vv[3]=sum(EN_Ind)
+vv[4]=sum(CS_Ind)
+vv[5]=sum(PS_Ind)
+vv[6]=sum(BS_Ind)
+vv[7]=sum(ED_Ind)
+vv[8]=sum(TOT_Ind)
+vv[9]=sum(vv[:8])
+uu[0]=sum(AH_Res)
+uu[1]=sum(CH_Res)
+uu[2]=sum(LS_Res)
+uu[3]=sum(EN_Res)
+uu[4]=sum(CS_Res)
+uu[5]=sum(PS_Res)
+uu[6]=sum(BS_Res)
+uu[7]=sum(ED_Res)
+uu[8]=sum(OTHER_Res)
+uu[9]=sum(uu[:9])
+#print(CH,CH.count(1),CH.count(0),len(CH))
+Income_Subjects={'Subjects':Subjects,'Income':[uu[0],uu[1],uu[2],uu[3],uu[4],uu[5],uu[6],uu[7],uu[8],uu[9]]}
+Industry_Subjects={'Subjects':Subjects,'Income':[vv[0],vv[1],vv[2],vv[3],vv[4],vv[5],vv[6],vv[7],vv[8],vv[9]]}
 results=pd.DataFrame({
 'Subjects':Subjects,
 'FTE_QS':u,
 'FTE_GRUP':v,
 'FTE_SUM':w
 })
-
+df3=pd.DataFrame(Income_Subjects)
+df4=pd.DataFrame(Industry_Subjects)
 # -----------------------
 # Display Results
 # -----------------------
 
-st.subheader("FTE Results")
+st.subheader("THE Results")
 st.dataframe(results)
-
+st.dataframe(df3)
+st.dataframe(df4)
 # -----------------------
 # Download Excel
 # -----------------------
@@ -198,11 +304,13 @@ st.dataframe(results)
 buffer = BytesIO()
 with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
     results.to_excel(writer, index=False, sheet_name="FTE Results")
-
+    df3.to_excel(writer,sheet_name='Research Income',index=False)
+    df4.to_excel(writer,sheet_name='Industry Income',index=False)
 st.download_button(
     label="Download Results as Excel",
     data=buffer,
-    file_name="FTE_results.xlsx",
+    file_name="THE_results.xlsx",
     mime="application/vnd.ms-excel"
 )
+
 
